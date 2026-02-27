247656b4865d6a2f47371d52f6acfef66701d5d1
Yash Padia <yashpadia001@gmail.com>
2026-02-27 12:40:01 +0530

    created the user/feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const { ConnectionRequest } = require("../modules/connectionRequest");
+const { UserModel } = require("../modules/user");
 
 const userRouter = express.Router();
 const USER_SAFE_DATA = "firstName lastName photoURL about age gender";
@@ -38,6 +39,36 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       return feild.fromUserId;
     });
     res.json({ message: "connections details", data });
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: "Error creating request.", error: err.message });
+  }
+});
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+    //   .populate("fromUserId", "firstName")  this is for only check who is sender the req
+    //   .populate("toUserId", "firstName");  this is for only check whom is sent
+
+    const hideUserFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUserFromFeed.add(req.fromUserId.toString());
+      hideUserFromFeed.add(req.toUserId.toString());
+    });
+
+    const usersData = await UserModel.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUserFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    }).select(USER_SAFE_DATA);
+
+    res.json({ message: "fetch proper done", usersData });
   } catch (err) {
     res
       .status(400)
